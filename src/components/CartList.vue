db8b2797335cf0b18e952053490e05d8ae98fa86
Liliana Matte <lilianamatte@MacBookPro.lan>
2026-03-09 12:46:54 -0500

    first commit

---
+++ src/components/CartList.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <CartListItem></CartListItem>
+        <CartListItem></CartListItem>
+    </div>
+</template>
+
+<script setup>
+import CartListItem from './CartListItem.vue';
+</script>
+
+<style scoped>
+
+</style>
