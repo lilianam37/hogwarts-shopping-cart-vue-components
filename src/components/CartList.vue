2507a0bb981f328f59152c7a883c7613d81e30c2
Liliana Matte <lilianamatte@MacBookPro.lan>
2026-03-09 13:36:35 -0500

    Pass data to child components with props

--- src/components/CartList.vue
+++ src/components/CartList.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
-        <CartListItem></CartListItem>
-        <CartListItem></CartListItem>
+     <CartListItem
+      v-for="cartItem in cartItems"
+      :key="cartItem.id"
+      :item="cartItem"></CartListItem>
     </div>
 </template>
 
 <script setup>
 import CartListItem from './CartListItem.vue';
+defineProps(['cartItems'])
 </script>
 
 <style scoped>
--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,17 +1,62 @@
 <template>
     <div>
-        <CartTitle></CartTitle>
+        <CartTitle :username="username"></CartTitle>
         <div class="cart-container">
-            <CartList class="cart-list"></CartList>
-            <OrderSummary class="order-summary"></OrderSummary>
+            <CartList class="cart-list" :cart-items="shoppingCartItems"></CartList>
+            <OrderSummary class="order-summary" :cart-items="shoppingCartItems" ></OrderSummary>
         </div>
     </div>
 </template>
 
 <script setup>
+import {ref } from 'vue'
 import CartTitle from './CartTitle.vue';
 import CartList from './CartList.vue';
 import OrderSummary from './OrderSummary.vue';
+
+let username = ref('Harry')
+let shoppingCartItems = ref([
+  {
+    id: 1,
+    productName: 'Dragon Liver',
+    price: 1500,
+    isInStock: true,
+    quantity: 3,
+    image: 'src/assets/img/DragonLiver.png'
+  },
+  {
+    id: 2,
+    productName: 'Golden Snitch',
+    price: 600,
+    isInStock: true,
+    quantity: 2,
+    image: ' src/assets/img/GoldenSnitch.png'
+  },
+  {
+    id: 3,
+    productName: 'Unicorn Tail Hair',
+    price: 1200,
+    isInStock: false,
+    quantity: 1,
+    image: 'src/assets/img/UnicornTailHair.png'
+  },
+  {
+    id: 4,
+    productName: 'Wand',
+    price: 2000,
+    isInStock: true,
+    quantity: 1,
+    image: 'src/assets/img/Wand.jpg'
+  },
+  {
+    id: 5,
+    productName: 'Nimbus 2000',
+    price: 5000,
+    isInStock: true,
+    quantity: 1,
+    image: 'src/assets/img/Nimbus2000.jpg'
+  }
+])
 </script>
 
 <style scoped>
